044cb44a5d4a7861c36f93088c1b56d7a07c9a61
bzmay0149 <mayjuanbaticono@gmail.com>
2023-03-20 11:27:45 +0100

    crear HeaderComp.spec.js y hacer 3 test

--- tests/unit/HeaderComp.spec.js
+++ tests/unit/HeaderComp.spec.js
@@ -4,9 +4,19 @@ import HeaderComp from "@/components/HeaderComp.vue";
 
 
 describe('HeaderComp', () => {
-    test('montar', () => {
-        const wrapper = mount(<HeaderComp />);
-        expect(wrapper.findAll('.nav-item')).toHaveLength(2);
+    it('montar', () => {
+        const wrapper = mount(<HeaderComp />); 
+        const navegador = wrapper.findAll('.nav-item');
+        expect(navegador).toHaveLength(2);
         
+    });
+    it('tiene un logo', () => {
+        const wrapper = mount(<HeaderComp />);
+        const logo = wrapper.find('.logo');
+        expect (logo).toBeTruthy();
+    });
+    it("Tiene un input", ()=>{
+        const wrapper= mount(<HeaderComp />);
+        expect(wrapper.find(".form-control").text()).toBe("")
     });
 })
