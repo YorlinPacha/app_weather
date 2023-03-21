26684abbc3610124f20ed1e76920dfff0013a8c5
Shamanesss <mentulbenodorna@gmail.com>
2023-03-21 23:14:52 +0100

    text de funcion

--- tests/unit/NavbarCom.spec.js
+++ tests/unit/NavbarCom.spec.js
@@ -16,5 +16,30 @@ describe('Header', () => {
     expect(links[0].text()).toContain('Semanal')
     expect(links[1].text()).toContain('Diario')
   })
+  
+
+
+
+  it(" doSearch funciona cuando submit el formulario", async () => {
+    const doSearchMock = jest.fn();
+    const wrapper = mount(HeaderComp, {
+   
+      setup() {
+        return {
+          doSearch: doSearchMock,
+        };
+      },
+    });
+
+    const input = wrapper.find("input[type='search']");
+    await input.setValue("Bilbao");
+
+    const form = wrapper.find("form");
+    await form.trigger("submit");
+
+    expect(doSearchMock).toHaveBeenCalled();
+  
+});
+
 
 })
