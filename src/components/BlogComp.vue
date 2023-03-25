1398b1a9956f210cb147951ba7131665639fdcfa
bzmay0149 <mayjuanbaticono@gmail.com>
2023-03-25 14:13:04 +0100

    hacer card en el blog

--- src/components/BlogComp.vue
+++ src/components/BlogComp.vue
@@ -1,10 +1,35 @@
 <template>
-  <h1>Blog</h1>
+  <h1 class="text-center">Blog</h1>
   <img v-if="isError" src="@/assets/logo.png" alt="error">
       <img v-if="isLoading" src="@/assets/logo.png" alt="loading">
       <div v-if="!isError && !isLoading" >
                 <div v-for="getNew in getNews" :key="getNew.id">
-                    <p>{{ getNew.name }}</p>
+                    <!-- <p>{{ getNew.name }}</p>
+                    <p>{{ getNew.fecha }}</p> -->
+                   <div class="d-flex justify-content-center">
+                    <div class="card-header">
+        <div class="row">
+            <div class="col-2 col-sm-2">
+                
+                 <img class="avatar-img" :src=" getNew.avatar " alt="Avatar de May" /> 
+            </div>
+            <div class="col-8 col-sm-8">
+                <h4 class="card-header-title m-0">
+                    
+                        {{ getNew.name }}
+                   
+                </h4>
+                <small>{{getNew.fecha}}</small>
+                <div class="foto-paisaje">
+                    <p>{{ getNew.location }}</p>
+                <img class="foto " :src=" getNew.foto " alt="fotos" />
+            </div>
+                <p>{{ getNew.comentario }}</p>
+            </div>
+        </div>
+            
+        </div>
+    </div>
                 </div>
 </div>
 </template>
@@ -74,5 +99,26 @@ async function getInfo(){
 </script>
 
 <style scoped>
+.avatar-img {
+ border-radius: 50%;
+  width: 40px;
+  height: 40px;
+  object-fit: cover;
+ } 
+.card-header-title {
+  line-height: 0.7;
+}
+.card-header-subTitle {
+  font-size: 0.8em;
+}
+  .card-header {
+    background-color: transparent;
+    padding: 0.5rem 1rem;
+    border-bottom: none;
+  }
+  .foto {
+    width: 18rem;
+  }
+
 
 </style>
