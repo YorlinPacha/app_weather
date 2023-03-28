e1bd0d75072f618b606798fca39ea7d5ba3d8b4e
xabier iglesias <xiauretamendi@gmail.com>
2023-03-28 09:59:58 +0200

    componente footer

---
+++ src/components/footer-vue.vue
@@ -0,0 +1,39 @@
+<script>
+
+</script>
+
+<template>
+   <div class="cajaGeneral">
+        <p class="info">Informacion</p>
+        <p class="copyright">copyright</p>
+        <p class="datos">Datos</p>
+
+    </div>
+</template>
+
+
+<style >
+.cajaGeneral{
+
+    display: flex;
+    justify-content: space-around;
+    background-color: #FFBFA9;
+    margin-bottom: 0;
+}
+
+.copyright{
+    font-size: 40px;
+    color: #424642;
+}
+.info{
+    display: flex;
+   align-items: center;
+   color:#424642;
+}
+
+.datos{
+    display: flex;
+   align-items: center;
+   color:#424642;
+}
+</style>
