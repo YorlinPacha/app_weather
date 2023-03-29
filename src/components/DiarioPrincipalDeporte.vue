b924d3d4c917b9600b3b291f20557f8adf2cb6de
trontzal <trontzal@gmail.com>
2023-03-29 12:08:55 +0200

    responsive y terminado pero sin filtros

--- src/components/DiarioPrincipalDeporte.vue
+++ src/components/DiarioPrincipalDeporte.vue
@@ -1,35 +1,48 @@
 <template>
     <!-- Solo se muestra si ninguna ciudad esta seleccionada -->
     <div v-if="climaActual == ''" class="climaPrincipalDiario">
-    Ninguna ciudad seleccionada
+        <img class="imagenGif"  src="../assets/img/entrada.gif" alt="">
     </div>
     <!-- div principal -->
     <div v-else class="climaPrincipalDiario">
-         <p class="ciudad"><img :src="`https://openweathermap.org/img/wn/${climaActual.weather[0].icon}@2x.png`" alt="icon">{{ climaActual.name }}</p>
-         <div class="containerDatos ">
-            <div class="grados">
+        <div class="titulo">
+            <p class="fecha">{{ obtenerFechaActual() }}</p>
+            <p class="ciudad">{{ climaActual.name }}</p>
+            <p class="hora">{{obtenerHoraActual()}}</p>
+        </div>
+        <div class="containerDatos ">
+            <div class="nubesGrados">
+                <img :src="`https://raw.githubusercontent.com/Shamanesss/app_weather/main/src/assets/img/weather_icons/${climaActual.weather[0].icon}.png`" alt="icon">
+                <div class="grados">
                     <p  v-if="climaActual.main == null">Error</p>
                     <p v-else>
                     {{Math.round(climaActual.main.temp)}}º
                     </p>
                 </div>
-             <div v-if="climaActual.weather == null">
+            </div>
+            <div v-if="climaActual.weather == null">
                 imagen.error
             </div>
                 <!-- <img class="bicicletas" src="../assets/img/fd4638442247e11430ff221fea8bed-unscreen.gif" alt=""> -->
-            <div v-else class="imagen">
-                <!-- <img class="cieloSolo" :src="`https://openweathermap.org/img/wn/${climaActual.weather[0].icon}@2x.png`" alt="icon"> -->
-                <img class="gif" src="../assets/img/fd4638442247e11430ff221fea8bed-unscreen.gif" alt="">
+            <div v-else class="gif">
+                <!-- <img class="gif" src="../assets/img/fd4638442247e11430ff221fea8bed-unscreen.gif" alt=""> -->
+                <img src="../assets/img/giphy.gif" alt="">
             </div>
                 <div class="datosEspecificos">
                     <p v-if="climaActual.main == null">Humedad:Error </p>
-                    <p v-else>Humedad:{{ climaActual.main.humidity }}% </p>
+                    <p v-else class="humedadimg" ><img src="../assets/img/kisspng-humidity-symbol-computer-icons-temperature-measure-against-vector-5ad99bf9116459.7880027515242106810713-removebg-preview.png" alt="" cl>{{ climaActual.main.humidity }}% </p>
                     <p v-if="climaActual.wind == null">Viento:error</p>
-                    <p v-else>Viento:{{ climaActual.wind.speed }}km/h</p>
+                    <p v-else class="vientoimg"><img src="../assets/img/png-transparent-wind-symbol-weather-map-computer-icons-weather-forecasting-wind-text-logo-weather-forecasting-removebg-preview.png" alt="" >{{ climaActual.wind.speed }}km/h</p>
                 </div>
         </div>
-        <p v-if="climaActual.main.temp >14 && climaActual.main.temp <24 && climaActual.main.humidity >=40 && climaActual.main.humidity <60 && climaActual.wind.speed <16">Momento perfecto para salir a hacer deporte(es un ejemplo)</p>
-        <p v-else>Otro (sin terminar)  </p>
+        <div class="recomendacion">
+            <div v-if="climaActual.main.temp >14 && climaActual.main.temp <24 && climaActual.main.humidity >=40 && climaActual.main.humidity <60 && climaActual.wind.speed <16">Momento perfecto para salir a hacer deporte(es un ejemplo)</div>
+
+            <div v-else>
+                <h2>Ejemplo de texto</h2>
+                <p>Ejemplo de mas texto pero mas pequeño que serian los detalles</p>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -42,8 +55,14 @@ defineProps({
 });
 
 
+function obtenerHoraActual() {
+    return new Date().toLocaleTimeString()
+}
 
 
+function obtenerFechaActual(){
+    return new Date().toLocaleDateString()
+}
 </script>
 
 <style scoped>
@@ -65,10 +84,15 @@ defineProps({
     box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
  }
 
+ .titulo{
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+ }
  .containerDatos{
     display: flex;
     align-items: center;
-    height: 14rem;
+    height: auto;
     position: relative;
     justify-content: space-evenly;
  }
@@ -80,6 +104,21 @@ defineProps({
     gap: 4rem;
  }
 
+ .datosEspecificos > p{
+    text-align: left;
+ }
+ .humedadimg{
+    margin-left: 1rem;
+ }
+
+.vientoimg > img{
+    width: 4rem;
+}
+
+.humedadimg > img{
+    width: 1.5rem;
+}
+
  .grados{
     place-self: center;
     font-size: 5rem;
@@ -89,17 +128,19 @@ defineProps({
     font-size: 2rem;
  }
 
- .gif{
-    width: 25rem;
+ .gif > img{
+    width: 15rem;
+    border-radius: 100%;
  }
- .cielo{
-    width: 20rem;
-    position: absolute;
-    bottom: 5rem;
- }
- .cieloSolo{
-    height: 15rem;
- }
+
+.recomendacion{
+    border-radius: 20px;
+    margin: 0 4rem 0 4rem;
+    z-index: 10;
+    background-color: white;
+    box-shadow: 2px 2px 2px 2px rgba(155, 153, 153, 0.438);
+}
+
 
  @media screen and (max-width: 900px){
     .climaPrincipalDiario{
@@ -112,12 +153,31 @@ defineProps({
         flex-direction: column;
     }
 
-    .datosClima{
-        width: 90%;
+    .nubesGrados{
+        display: flex;
+        gap: 4rem;
+        flex-direction: row-reverse;
     }
-    .imagen{
-        width: 100%;
-        position: relative;
+
+    .fecha{
+        display: none;
+    }
+    .hora{
+        display: none;
+    }
+
+    .datosEspecificos{
+    gap: 0rem;
+    flex-direction: row;
+    align-items: center;
+    }
+
+    .gif > img{
+    width: 12rem;
+    }
+
+    .recomendacion{
+        margin:0;
     }
  }
 
