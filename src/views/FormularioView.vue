ac803644a9dacd00b76c3f82bd6980d75454c889
bzmay0149 <mayjuanbaticono@gmail.com>
2023-03-29 11:36:56 +0200

    formulario con crud

--- src/views/FormularioView.vue
+++ src/views/FormularioView.vue
@@ -2,12 +2,14 @@
   <div>
 <PostComp />
 <DelComp  />
+<UpdateComp />
   </div>
 </template>
 
 <script setup>
 import PostComp from '@/components/PostComp.vue'
 import DelComp from '@/components/DelComp.vue'
+import UpdateComp from '@/components/UpdateComp.vue';
 
 
 </script>
