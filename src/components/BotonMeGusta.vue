d834d9f892b739183e9172671e79d6e207a48099
Shamanesss <mentulbenodorna@gmail.com>
2023-03-30 20:52:54 +0200

    add function of favorites

--- src/components/BotonMeGusta.vue
+++ src/components/BotonMeGusta.vue
@@ -2,34 +2,36 @@
     <div class="boton">
        
     <button class="MeGusta" @click="megustas"><img class="like" src="https://img.icons8.com/color/1x/facebook-like--v3.gif" alt=""> </button>
-    <button class="favorito vacio"><img  src="https://cdn-icons-png.flaticon.com/32/535/535285.png" alt=""> </button>
-    <button class="favorito"><img src="https://cdn-icons-png.flaticon.com/32/210/210545.png" alt=""> </button>
+    <button  class="favorito vacio" v-show="!isFavorito" @click="addFavorite"><img  src='https://cdn-icons-png.flaticon.com/32/535/535285.png' alt=""> </button>
+    <button class="favorito" ><img v-show="isFavorito" @click="addFavorite" src='https://cdn-icons-png.flaticon.com/32/210/210545.png' alt=""> </button>
     <p>Me gusta {{ megusta }}</p>
  </div>
 </template>
 
 <script>
-import{ ref }from "vue";
+import{ ref}from "vue";
  export default {
     name: "BotonMeGusta",
-    // props: {
-    //     //Type control style
-    //     type: {
-    //         type:String,
-    //         default:"default"
-    //     },
-
-    // },
+    
    setup(){
+    let isFavorito = ref(false)
     let megusta = ref(0);
     const megustas =()=>{
         console.log("me gusta");
         console.log(megusta)
         megusta.value++;
     }
+   
+    const addFavorite = function () {
+        isFavorito.value = !isFavorito.value
+      }
+  
+     
     return{
         megusta,
-        megustas
+        megustas,
+        isFavorito,
+        addFavorite   
     }
 
    },
