0383b222df0740325c74e56442a327925ed3e01e
Shamanesss <mentulbenodorna@gmail.com>
2023-03-30 12:53:52 +0200

    funcion del boton like

--- src/components/BotonMeGusta.vue
+++ src/components/BotonMeGusta.vue
@@ -1,19 +1,39 @@
 <template>
     <div class="boton">
        
-    <button class="MeGusta" ><img class="like" src="https://img.icons8.com/color/1x/facebook-like--v3.gif" alt=""> </button>
+    <button class="MeGusta" @click="megustas"><img class="like" src="https://img.icons8.com/color/1x/facebook-like--v3.gif" alt=""> </button>
     <button class="favorito vacio"><img  src="https://cdn-icons-png.flaticon.com/32/535/535285.png" alt=""> </button>
     <button class="favorito"><img src="https://cdn-icons-png.flaticon.com/32/210/210545.png" alt=""> </button>
-    <p>Me gusta 0</p>
+    <p>Me gusta {{ megusta }}</p>
  </div>
 </template>
 
 <script>
-export default {
-  name: 'BotonMeGusta',
-  components: {
-  },
-}
+import{ ref }from "vue";
+ export default {
+    name: "BotonMeGusta",
+    // props: {
+    //     //Type control style
+    //     type: {
+    //         type:String,
+    //         default:"default"
+    //     },
+
+    // },
+   setup(){
+    let megusta = ref(0);
+    const megustas =()=>{
+        console.log("me gusta");
+        console.log(megusta)
+        megusta.value++;
+    }
+    return{
+        megusta,
+        megustas
+    }
+
+   },
+ }
 </script>
 
 <style scoped>
