6c8f578032de7651b0cb06386fddd44bcd580dcb
YorlinPacha <yorlin1517@gmail.com>
2023-04-03 17:52:42 +0200

    Mejorar espaciado del componente horas en la vista daily

--- src/views/DailyDeporteView.vue
+++ src/views/DailyDeporteView.vue
@@ -9,7 +9,7 @@
         :climaCompleto="climaCompleto"
       />
       <br />
-      <ClimaHorasDia
+      <ClimaHorasDia class="cuadroHorasDia"
         :climaActual="climaActual"
         :climaCompleto="climaCompleto"
       />
@@ -84,7 +84,7 @@ const noMeGusta = ()=>{
   border-radius: 10px;
   border: 1px solid rgba(255, 255, 255, 0.18);
 
-  max-width: 80px;
+  max-width: 90px;
   text-align: center;
   padding: 0.5rem 1rem ;
   margin: 0 auto 0 90%;
@@ -95,6 +95,10 @@ const noMeGusta = ()=>{
   font-size: 1rem;
   color: #fff;
 }
+.cuadroHorasDia{
+margin: 4rem 0;
+}
+
 
 
 /* -------------------Responsive Tablet ----------------*/
