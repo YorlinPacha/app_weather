a7af1548238a6cdca904b33c0b6d84af62369792
Jonathaneche <jonathan.eche@gmail.com>
2023-04-05 09:56:14 +0200

    Page not found modificado

--- src/views/PageNotFound.vue
+++ src/views/PageNotFound.vue
@@ -1,18 +1,32 @@
 <template>
-    <div>
-        <NotFoundImg />
+    
+
+    <div class="contenedor404">
+        <img src="../assets/img/404.png" alt="">
     </div>
     
 </template>
 
 <script setup>
-import NotFoundImg from '@/components/NotFoundImg.vue';
+
 
 
 </script>
 
 <style scoped>
-div{
+
+.contenedor404{
+    width: 90vw;
+    margin: 0 auto;
+    display: flex;
+    align-items: center;
+    justify-content: center;
     min-height: 100vh;
+    
+}
+
+img{
+    
+    width: 45%;
 }
 </style>
